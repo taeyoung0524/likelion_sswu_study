54ddcd727fb439c316a899de365a7d4039a1a7c6
taeyoung0524 <miseoseo1@naver.com>
2025-05-18 21:31:12 +0900

    스터디 3주차

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Routes } from 'react-router-dom'
+import Home from "./Pages/Home/Home.jsx"
+import Video from "./Pages/Video/Video.jsx"
+import { Route } from 'react-router-dom'
+import { useState } from 'react'
 
 const App = () => {
+
+  const [sidebar, setSidebar] = useState(true);
+
   return (
     <div>
-      <Navbar/>
+      <Navbar setSidebar={setSidebar}/>
+      <Routes>
+        <Route path="/" element={<Home sidebar={sidebar}/>}/>
+        <Route path="/video/:categoryId/:videoId" element={<Video/>}/>
+      </Routes>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,16 +8,18 @@ import more_icon from "../../assets/more.png"
 import notification_icon from "../../assets/notification.png"
 import profile_icon from "../../assets/마루.jpeg"
 
-const Navbar = () => {
+const Navbar = ({setSidebar}) => {
   return (
     <nav className="flex-div">
       <div className="nav-left flex-div">
-        <img className="menu-icon" src={menu_icon} alt="메뉴 아이콘"/>
+        <img className="menu-icon" onClick ={() => setSidebar(prev => prev === false? true : false)} src={menu_icon} alt="메뉴 아이콘"/>
         <img className="logo" src={logo} alt="로고 아이콘"/>
       </div>
       <div className="nav-middle flex-div">
-        <input type="text" placeholder='Search'/>
-        <img src={search_icon} alt="검색 아이콘"/>
+        <div className="search-box flex-div">
+          <input type="text" placeholder='Search'/>
+          <img src={search_icon} alt="검색 아이콘"/>
+        </div>
       </div>
       <div className="nav-right flex-div">
         <img src={upload_icon} alt=""/>
