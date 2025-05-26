631c9e2351a86ce6fc0e8a65c8d96f142e1d57f9
taeyoung0524 <miseoseo1@naver.com>
2025-05-26 17:40:59 +0900

    API url 오타 수정

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -23,7 +23,7 @@ const Feed = ({category}) => {
     <div className="feed">
         {data.map((item, index)=> {
             return (
-                <Link to ={`video/${item.snippet.categoryId}/{item.id}`}  key={item.id}  className="card">
+                <Link to ={`video/${item.snippet.categoryId}/${item.id}`}  key={item.id}  className="card">
                     <img src={item.snippet.thumbnails.medium.url} alt="" />
                     <h2>{item.snippet.title}</h2>
                     <h3>{item.snippet.channelTitle}</h3>
